b0a1ee997faeae27c06f7edef053758b6fc78f53
CynthiaKamau <cynthiakamau54@gmail.com>
2020-10-08 21:33:59 +0300

    Bug fix

--- map2.js
+++ map2.js
@@ -4,87 +4,101 @@ var driver_name_2 = "Neil Doer";
 var cancelled = "Trip cancelled, please wait as we connect you to another driver";
 var image = 'https://images.sendyit.com/web_platform/vendor_type/top/2.svg';
 
-var first_interval = window.onload = function(){
-    setTimeout(initMap1, 15000);
-  };
+window.onload = function(){  
 
-  var second_interval = window.onload = function(){
-    setTimeout(initMap2, 25000);
-  };  
+        setTimeout(initMap1, 15000);
 
-let counter = 0; 
+        setTimeout(initMap2, 25000);
 
-function initMap() {
+}  
+  
+    function initMap() {
 
-    var myOptions = {
-        zoom: 16,
-        center: new google.maps.LatLng(-1.300355, 36.773850),
-        mapTypeId: "roadmap"
-    };    
+        var myOptions = {
+            zoom: 16,
+            center: new google.maps.LatLng(-1.300355, 36.773850),
+            mapTypeId: "roadmap"
+        };    
 
-    map = new google.maps.Map(document.getElementById('map_canvas_2'), myOptions);
-
-    marker = new google.maps.Marker({
-        position: new google.maps.LatLng(-1.300355, 36.773850),
-        title: "Your driver",
-        visible: true,
-        map: map,
-        icon: image,
-        });
-
-        var infowindow = new google.maps.InfoWindow({
-            content: '<div><div>' + driver_name_1 + ' </div><div>' + marker.position + '</div></div>'
-        });
-    
-        infowindow.open(map,marker);
-        
-  }
-
-  function initMap1() {
-
-    var myOptions = {
-        zoom: 16,
-        center: new google.maps.LatLng(-1.300355, 36.773850),
-        mapTypeId: "roadmap"
-    };    
-
-    map = new google.maps.Map(document.getElementById('map_canvas_2'), myOptions);
-
-        var infowindow = new google.maps.InfoWindow({
-            content: '<div><div>' + cancelled + ' </div><div>' 
-        });
-    
-        infowindow.open(map);
-
-  }  
-
-  function initMap2() {
-
-    var myOptions = {
-        zoom: 16,
-        center: new google.maps.LatLng(-1.300355, 36.773850),
-        mapTypeId: "roadmap"
-    };    
-
-    map = new google.maps.Map(document.getElementById('map_canvas_2'), myOptions);
+        map = new google.maps.Map(document.getElementById('map_canvas_2'), myOptions);
 
         marker = new google.maps.Marker({
-            position: new google.maps.LatLng(-1.291879, 36.778389),
+            position: new google.maps.LatLng(-1.300355, 36.773850),
             title: "Your driver",
             visible: true,
             map: map,
             icon: image,
-        });
+            });
 
-        var infowindow = new google.maps.InfoWindow({
-            content: '<div><div>' + driver_name_2 + ' </div><div>' + marker.position + '</div></div>'
-        });
+            var infowindow = new google.maps.InfoWindow({
+                content: '<div><div>' + driver_name_1 + ' </div><div>' + marker.position + '</div></div>'
+            });
+        
+            infowindow.open(map,marker);
+            
+    }
+
+    function initMap1() {
+
+        var myOptions = {
+            zoom: 16,
+            center: new google.maps.LatLng(-1.300355, 36.773855),
+            mapTypeId: "roadmap"
+        };    
     
-        infowindow.open(map,marker);
+        map = new google.maps.Map(document.getElementById('map_canvas_2'), myOptions);
+    
+            marker = new google.maps.Marker({
+            position: new google.maps.LatLng(-1.300355, 36.773855),
+            title: "Your driver",
+            visible: true,
+            map: map,
+            });
+    
+            var infowindow = new google.maps.InfoWindow({
+                content: '<div><div>' + cancelled + ' </div><div>' 
+            });
+    
+            infowindow.open(map);
+        
+    }  
+    
+    function initMap2() {
+    
+        var myOptions = {
+            zoom: 16,
+            center: new google.maps.LatLng(-1.300355, 36.773850),
+            mapTypeId: "roadmap"
+        };    
+    
+        map = new google.maps.Map(document.getElementById('map_canvas_2'), myOptions);
+    
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(-1.291879, 36.778389),
+                title: "Your driver",
+                visible: true,
+                map: map,
+                icon: image,
+            });
+    
+            var infowindow = new google.maps.InfoWindow({
+                content: '<div><div>' + driver_name_2 + ' </div><div>' + marker.position + '</div></div>'
+            });
+    
+            infowindow.open(map,marker);
+        
+    } 
+      
+   
 
-  }  
 
-initMap();  
+
+
+
+  
+
+
+
 
  
 
